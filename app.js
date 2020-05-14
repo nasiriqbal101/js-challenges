e28d8db9ccecc3bf064d77007504ed6f52e6b063
nasiriqbal101 <nasiriqbal101@yahoo.com>
2020-05-14 23:38:21 +0100

    js practice

--- app.js
+++ app.js
@@ -2,14 +2,21 @@
 // we dont have .reverse for a string but we have this function fro array
 // commented method 1 is a simple long way for beginner to understand every step
 //second way to bring all the method in return 
+// third method is applying for loop, since we want to reverse, so we will apply decending for loop
 
 reverseString = (str) => {
-//     let strArr = str.split("");
-//     let reverseStrArray = strArr.reverse();
-//     let reverseStr = reverseStrArray.join('');
-// return reverseStr
+    //     let strArr = str.split("");
+    //     let reverseStrArray = strArr.reverse();
+    //     let reverseStr = reverseStrArray.join('');
+    // return reverseStr
 
-return str.split("").reverse().join("");
+    // return str.split("").reverse().join("");
+
+    let final = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        final += str[i]
+    }
+    return final;
 }
 console.log(reverseString("hello"));
 
