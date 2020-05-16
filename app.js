e9b04cb655fd455a320b9f90bb4aafe1e0853770
nasiriqbal101 <nasiriqbal101@yahoo.com>
2020-05-16 23:47:30 +0100

    js challenges

--- app.js
+++ app.js
@@ -4,30 +4,30 @@
 //second way to bring all the method in return 
 // third method is applying for loop, since we want to reverse, so we will apply decending for loop
 
-// reverseString = (str) => {
-//     //     let strArr = str.split("");
-//     //     let reverseStrArray = strArr.reverse();
-//     //     let reverseStr = reverseStrArray.join('');
-//     // return reverseStr
+reverseString = (str) => {
+    //     let strArr = str.split("");
+    //     let reverseStrArray = strArr.reverse();
+    //     let reverseStr = reverseStrArray.join('');
+    // return reverseStr
 
-//     // return str.split("").reverse().join("");
+    // return str.split("").reverse().join("");
 
-//     let final = "";
-//     for (let i = str.length - 1; i >= 0; i--) {
-//         final += str[i]
-//     }
-//     return final;
-// }
-// console.log(reverseString("hello"));
+    let final = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        final += str[i]
+    }
+    return final;
+}
+console.log(reverseString("hello"));
 
 // 2- Factorialize a number
 
 factorialise = (num) => {
     let result = 1;
-    for ( let i = 1; i <=num; i++){
+    for (let i = 1; i <= num; i++) {
         result = result * i;
     }
-return result;
+    return result;
 }
 console.log(factorialise(5));
 
