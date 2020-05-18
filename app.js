8a4a7e316092c725c58e323fe11df737d3e09e81
nasiriqbal101 <nasiriqbal101@yahoo.com>
2020-05-18 23:32:40 +0100

    js challenges

--- app.js
+++ app.js
@@ -31,5 +31,15 @@ factorialise = (num) => {
 }
 console.log(factorialise(5));
 
+//3 - check for Palindromes
+
+palindrome = (str) => {
+    let reversed = str.split('').reverse().join("");
+    if (reversed === str) return true;
+    
+    return false;
+}
+console.log(palindrome('eyeeee'));
+
 
 
