329583b1b66e39357df99879f01c413c6dc1cfb9
nasiriqbal101 <nasiriqbal101@yahoo.com>
2020-05-26 23:27:09 +0100

    js challenges

--- app.js
+++ app.js
@@ -62,12 +62,23 @@ console.log(findLongestWord("Today was a lovely day"));
 
 //5 -titleCase a string
 
+//1st solution
+// titleCase = (str) => {
+//     let words = str.toLowerCase().split(" ");
+//     for (let i = 0; i < words.length; i++) {
+//         words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+//     }
+//     return words.join(" ");
+// }
+// console.log(titleCase("Today is sunday"));
+
+//2nd solution
+
 titleCase = (str) => {
-    let words = str.toLowerCase().split(" ");
-    for (let i = 0; i < words.length; i++) {
-        words[i] = words[i][0].toUpperCase() + words[i].slice(1);
-    }
-    return words.join(" ");
+    let titled = str.toLowerCase().split(" ").map(function(elem) {
+        return elem[0].toUpperCase() + elem.slice(1);
+    })
+    return titled.join(" ");
 }
 console.log(titleCase("Today is sunday"));
 
