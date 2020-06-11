660d4738e74cdb6ebc0e02681990ea2e02b6319e
nasiriqbal101 <nasiriqbal101@yahoo.com>
2020-06-11 23:02:27 +0100

    js

--- app.js
+++ app.js
@@ -119,3 +119,13 @@ confirmEnding = (str, target) => {
 }
 
 console.log(confirmEnding("Hello", "o"))
+
+//8 repeat string num times
+
+repeatStringNumTimes = (str, num) => {
+if (num < 0) return "";
+return str.repeat(num)
+}
+
+console.log(repeatStringNumTimes("abc", 3))
+
